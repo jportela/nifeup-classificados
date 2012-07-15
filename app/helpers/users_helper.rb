f63ef7b6af0cf94b43c1b195311e4c42d43150b0
Eduardo Pinto <efgpinto@gmail.com>
2012-07-15 01:44:14 +0100

    bug fix: session on, database clear - login issue

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,14 @@
 module UsersHelper
   def admin?
+    puts "DFDSFSDFSDFSDF\n\n\n"
+    puts session.inspect
     return false unless session[:user_id]
-    user = User.find(session[:user_id])
+    
+    begin
+        user = User.find(session[:user_id])
+        rescue ActiveRecord::RecordNotFound
+            return false
+    end
     return user.admin
   end
 end
